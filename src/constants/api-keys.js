58d1b471310dfcd8a119d5ba38e3f7bb96bd66da
Emir Alicic <emir@gradient.ba>
2023-11-22 09:16:00 +0100

    Comment around api-keys added and test-data.js removed.

--- src/constants/api-keys.js
+++ src/constants/api-keys.js
@@ -1,5 +1,12 @@
-
-
+/**
+ * Just for the Article presentation purposes.
+ *
+ * In one of the future articles, this will be changed and properly (safely) configured.
+ *
+ * NOTE: Keys shared here were once valid, so you can expect to get similar keys in length and format sense.
+ *
+ * @type {{WooCommerceConsumerSecret: string, WooCommerceConsumerKey: string, Webflow: string}}
+ */
 export const ApiKeys = {
     Webflow: 'd0195eb377b5ac7902de29471583fb46f69ba57367fd8fb53cfc85ca875bb6c9',
     WooCommerceConsumerKey: 'ck_c801debae8013f31da0f961ce1084e32f52937a7',
